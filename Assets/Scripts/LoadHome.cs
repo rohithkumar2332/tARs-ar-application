3f12aef676e0f28019582166becbd15d34011ed9
Rohith Kumar R <rohithkumar2332@gmail.com>
2020-04-05 13:13:10 +0530

    Full Project Upload
    In this commit, I'm uploading the complete Working Project to the GitHUb.

---
+++ Assets/Scripts/LoadHome.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LoadHome : MonoBehaviour
+{
+    public void SceneLoader(int SceneIndex)
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void chk1()
+    {
+        Debug.Log("checking function1.....");
+    }
+}
---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LoadScene : MonoBehaviour
+{
+
+    public void SceneLoader(int SceneIndex)
+    {
+        SceneManager.LoadScene(SceneIndex);
+    }
+    public void chk()
+    {
+        Debug.Log("checking function.....");
+    }
+}
